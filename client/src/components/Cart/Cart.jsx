f245b02fe60d9d9d8c7a06aa2c3e398a73b1dd46
Studawatta <st.udawatte@gmail.com>
2023-08-12 22:55:42 +0530

    cart component created

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import './Cart.scss'
 
 const Cart = () => {
@@ -7,6 +8,7 @@ const Cart = () => {
       img: 'https://cdn.cafecoton.com/Delivery/Public/Image/600x-1/pr-t-shirt-andrew-1-e23_1.jpeg',
       img2: 'https://cdn.endource.com/image/s3-884c6162a7c2550c266dbce364ab2ff9/detail/mango-stretch-cotton-t-shirt.jpg',
       title: 'Long Sleeve Graphic T-shirt',
+      desc: 'some description about product',
       isNew: true,
       oldPrice: 19,
       price: 12,
@@ -16,6 +18,7 @@ const Cart = () => {
       img: 'https://cdn.cafecoton.com/Delivery/Public/Image/600x-1/pr-t-shirt-andrew-1-e23_1.jpeg',
       img2: 'https://cdn.endource.com/image/s3-884c6162a7c2550c266dbce364ab2ff9/detail/mango-stretch-cotton-t-shirt.jpg',
       title: 'Long Sleeve Graphic T-shirt',
+      desc: 'some description about product',
       isNew: true,
       oldPrice: 19,
       price: 12,
@@ -29,10 +32,19 @@ const Cart = () => {
           <img src={item.img} alt="img" />
           <div className="details">
             <h1>{item.title}</h1>
-            <p>{item.desc.substring(0, 100)}</p>
+            <p>{item.desc?.substring(0, 30)}</p>
+            <div className="price">1 x ${item.price}</div>
           </div>
+          <DeleteOutlinedIcon className="delete" />
         </div>
       ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$124</span>
+      </div>
+
+      <button>PROCEED TO CHECKOUT</button>
+      <span className="reset">Reset Cart </span>
     </div>
   )
 }
